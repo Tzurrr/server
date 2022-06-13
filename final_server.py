f56897c43aa10232720946ad4f8f101a74c8f029
Ubuntu <tzur@HAProxy-server1.csg2ojf14x1ufinqtxn5hek11f.ax.internal.cloudapp.net>
2022-06-13 08:58:29 +0000

    Moved merging logics into a seperate module

--- final_server.py
+++ final_server.py
@@ -5,23 +5,17 @@ import find_dot
 import encryptor
 import elogger
 import os
+import file_merger
 
 app = FastAPI()
 
 @app.post("/")
 async def upload(files: List[UploadFile] = File(...)):
-    contents = []
-    for file in files:
-        contents.append(await file.read())
-        await file.close()
-    with open (f"/home/tzur/all-the-photos/{os.path.splitext(file.filename)[0]}.jpg", "wb") as file:
-        file.writelines(contents)
-        filename = file.name
+    filename = file_merger.merge_halfs(files)
     elogger.write_logs_to_elastic("wrote")
     encryptor.encrypt(filename)
     return {"message": f"Successfuly uploaded {[file.filename for file in files]}"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
-    intro = "/home/tzur/getters/"
 
