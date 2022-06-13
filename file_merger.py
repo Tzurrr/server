e95dcb2d4ef590ef8b17e643f1f7dde215fc97b1
Tzurrr <zur001@gmail.com>
2022-06-13 13:42:44 +0000

    Deleted unnecessary files

--- file_merger.py
+++
@@ -1,9 +0,0 @@
-def merge_halfs(files):
-    contents = []
-    for file in files:
-        contents.append(await file.read())
-        await file.close()
-    with open (f"/home/tzur/all-the-photos/{os.path.splitext(file.filename)[0]}.jpg", "wb") as file:
-        file.writelines(contents)
-        filename = file.name
-    return filename
