4a8bc009b92d78e687aacf54990661430e879a1f
Ubuntu <tzur@HAProxy-server1.csg2ojf14x1ufinqtxn5hek11f.ax.internal.cloudapp.net>
2022-06-13 08:26:55 +0000

    changed the func name to a more meaningful one and removed commented lines

--- elogger.py
+++ elogger.py
@@ -4,27 +4,23 @@ import ecs_logging
 import requests
 import uuid
 import os
+import json_parser
 
-def write(event_string):
+def write_logs_to_elastic(event_string):
+    conf_dict = json_parser.parse_json_to_var("/home/tzur/client/config.json")
+    url_path = conf_dict["kibanas_url"]
     logger = logging.getLogger("app")
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler('/home/tzur/elvis.json')
+    handler = logging.FileHandler(conf_dict["logfile_path"])
     handler.setFormatter(ecs_logging.StdlibFormatter())
     logger.addHandler(handler)
     json_UUID = uuid.uuid4()
-    keywords_arr = ["arrivedtoserver", "sent", "didntsent", "wrote"]
     
-    # TODO: add value check
-
     logger.info(event_string, extra={"http.request.method": "get", "UUID": json_UUID})
-    log_content = open("/home/tzur/elvis.json", "r")
-    log_content_json = json.load(log_content)
-    log_content.close()
+    log_json = json_parser.parse_json_to_var(conf_dict["logfile_path"])
 
-    os.remove("/home/tzur/elvis.json")
+    os.remove(conf_dict["logfile_path"])
 
     doc_UUID = uuid.uuid4()
-    resp = requests.put(url=f"http://13.81.211.207:9200/{event_string}/_doc/{doc_UUID}", json=log_content_json,
+    resp = requests.post(url=f"{url_path}/{event_string}/_doc/{doc_UUID}", json=log_json,
                         headers={'Content-Type': 'application/json'})
-    #print(resp.json())
-
