b6c47dbc8f366e397ce51dca522e4eac18051d32
Tzurrr <zur001@gmail.com>
2022-06-19 18:53:33 +0000

    changed logics so theres no new file created each time

--- elogger.py
+++ elogger.py
@@ -1,32 +1,22 @@
 import logging
-import ecs_logging
-import requests
-import uuid
-import os
+import sys
+from python_elastic_logstash import ElasticHandler, ElasticFormatter
 import json_parser
 
-
 def write_logs_to_elastic(event_string):
-    conf_dict = json_parser.parse_json_to_var("/home/tzur/server/config.json")
+    conf_dict = json_parser.parse_json_to_var("/home/tzur/client/config.json")
     url_path = conf_dict["kibanas_url"]
-    logfile_path = conf_dict["logfile_path"]
 
-    if len(url_path) == 0:
-        return
-
-    # configure logger
-    logger = logging.getLogger("app")
+    logger = logging.getLogger(event_string)
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(conf_dict["logfile_path"])
-    handler.setFormatter(ecs_logging.StdlibFormatter())
-    logger.addHandler(handler)
 
-    # output to the local log
-    logger.info(event_string, extra={"http.request.method": "get"})
+    elastic_handler = ElasticHandler(url_path)
+    elastic_handler.setFormatter(ElasticFormatter())
 
-    # send to elastic
-    log_json = json_parser.parse_json_to_var(logfile_path)
-    os.remove(logfile_path)
-    doc_uuid = uuid.uuid4()
-    requests.post(url=f"{url_path}/{event_string}/_doc/{doc_uuid}", json=log_json,
-                  headers={'Content-Type': 'application/json'})
+    logger.addHandler(elastic_handler)
+
+    logger.info(event_string)
+
+
+
+write_logs_to_elastic("arr")
